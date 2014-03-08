15ad8c69704c4530dab22cd38671b33cb342417f
user <user@users-Mac.local>
2014-03-08 21:39:38 +1000

    Initial Commit

---
+++ twofie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twofie
+//
+//  Created by user on 3/8/14.
+//  Copyright (c) 2014 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
